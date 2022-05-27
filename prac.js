f698a376ca7d113c81581ef05bfe679ae4c13722
Moscolape <chunkums088@gmail.com>
2022-05-27 13:45:09 +0100

    initial commit

---
+++ prac.js
@@ -0,0 +1,123 @@
+// let Surname = "Chukwunenye"
+// let Firstname = "Moses"
+// let Fullname = Surname+","+ " "+ Firstname
+// console.log(Fullname)
+
+// let num = 3
+// let score = 5
+// let total = num / score * 2
+// console.log(total)
+
+// let fahrenheit = 149
+// let celsius = (fahrenheit - 32) * 5/9
+// let kelvin = (fahrenheit + 459.67) *5/9
+
+// console.log(celsius)
+// console.log(kelvin)
+
+// let temp = 92
+
+// if (temp < 32){
+//     console.log('The weather is cold today')
+// } else if(temp >= 32 && temp <= 70){
+//     console.log(`Today's weather is nice`)
+// } else{
+//     console.log('The weather is hot today')
+// }
+
+// let age = 21
+
+// if(age <= 12){
+//     console.log(`He's just a child`)
+// } else if (age > 12 && age < 21){
+//     console.log(`He's just a teenager`)
+// }else{
+//     console.log(`He's just a man`)
+// }
+
+// let varOne = 'varOne'
+
+// if(true){
+//     console.log(varOne)
+//     let varTwo = 'varTwo'
+//     console.log(varTwo)
+
+//     if (true){
+//         let varFour = 'varFour'
+//     }
+// }
+
+// if (true){
+//     let varThree = 'varThree'
+// }
+// // console.log(varTwo)
+
+// let greetUser = function(){
+//     console.log('Welcome User')
+// }
+
+// greetUser()
+// greetUser()
+// greetUser()
+
+// let square = function(num){
+//     let result = num * num
+//     return result
+// }
+
+// let value = square(13)
+// let otherValue = square(25)
+
+// console.log(value)
+// console.log(otherValue)
+
+// let convertFahrenheitToCelsius = function(fahrenheit){
+//     let celsius = (fahrenheit - 32) * 5/9
+//     return celsius
+// }
+
+// let tempOne = convertFahrenheitToCelsius(50)
+// let tempTwo = convertFahrenheitToCelsius(167)
+
+// console.log(tempOne)
+// console.log(tempTwo)
+
+// let rage = 27
+// age = undefined
+
+// console.log(age)
+
+// let add = function(a,b,c){
+//     let result = a*b+c
+//     console.log(result)
+// }
+
+// add(3,5,4)
+
+// let getScoreText = function(name,score){
+//     console.log(name)
+//     console.log(score)
+// }
+
+// getScoreText('Mosco', '85')
+
+// let getTip = function(total, tipscore = .2){
+//     return total = tipscore
+// }
+
+// let tip = getTip(100, .25)
+// console.log(tip)
+
+let a = [1,2,3]
+let b = [1,2,3]
+let y= 31
+
+for(i = 0; i < a.length; i++){
+    let c = a[0].toString()
+    let d = b[b.length-1].toString()
+    
+    let e = c.concat(d);
+    let f = [];
+    let g = f.push(e)
+    console.log(e);
+}
